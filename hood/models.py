126cd0ee79b861c1aba62c43df874238872936f9
TheCaffeine <85110314+TheCaffeine@users.noreply.github.com>
2022-01-10 19:37:04 +0300

    Update models.py

--- hood/models.py
+++ hood/models.py
@@ -1,9 +1,17 @@
+from django.db import models
+from django.contrib.auth.models import User
+from pyuploadcare.dj.models import ImageField
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+
+
 class NeighbourHood(models.Model):
     name = models.CharField(max_length=50)
     location = models.CharField(max_length=60)
     admin = models.ForeignKey("Profile", on_delete=models.CASCADE, related_name='hood')
     hood_logo = models.ImageField(upload_to='images/')
     description = models.TextField()
+    
      def __str__(self):
          return f'{self.name} hood'
 
