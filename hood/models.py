97babea4de5e035148cffb35b10776617f4d42f1
TheCaffeine <85110314+TheCaffeine@users.noreply.github.com>
2022-01-10 19:33:41 +0300

    Update models.py

--- hood/models.py
+++ hood/models.py
@@ -1,8 +1,3 @@
-from django.db import models
-from django.contrib.auth.models import User
-from pyuploadcare.dj.models import ImageField
-from django.dispatch import receiver
-from django.db.models.signals import post_save
 class NeighbourHood(models.Model):
     name = models.CharField(max_length=50)
     location = models.CharField(max_length=60)
@@ -12,37 +7,37 @@ class NeighbourHood(models.Model):
      def __str__(self):
          return f'{self.name} hood'
 
+     def create_neighborhood(self):
+         self.save()
+
+     def delete_neighborhood(self):
+         self.delete()
+
+     @classmethod
+     def find_neighborhood(cls, neighborhood_id):
+         return cls.objects.filter(id=neighborhood_id)
+
 
  class Profile(models.Model):
-     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
-
-    
-          
-            
-    
-
-          
-          
-            
-    
-
-          
-    
-    @@ -46,5 +56,16 @@ class Business(models.Model):
-  
+ 
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
     name = models.CharField(max_length=80, blank=True)
     bio = models.TextField(max_length=254, blank=True)
     profile_picture = models.ImageField(upload_to='images/', default='default.png')
     neighbourhood = models.ForeignKey(NeighbourHood, on_delete=models.SET_NULL, null=True, related_name='members', blank=True)
+    
     def __str__(self):
         return f'{self.user.username} profile'
+        
     @receiver(post_save, sender=User)
     def create_user_profile(sender, instance, created, **kwargs):
         if created:
             Profile.objects.create(user=instance)
+            
     @receiver(post_save, sender=User)
     def save_user_profile(sender, instance, **kwargs):
         instance.profile.save()
+        
 class Business(models.Model):
     name = models.CharField(max_length=120)
     email = models.EmailField(max_length=254)
@@ -51,3 +46,13 @@ class Business(models.Model):
     user = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='owner')
      def __str__(self):
          return f'{self.name} Business'
+
+     def create_business(self):
+         self.save()
+
+     def delete_business(self):
+         self.delete()
+
+     @classmethod
+     def find_business(cls, business_id):
+         return cls.objects.filter(id=business_id)
