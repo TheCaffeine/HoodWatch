01143b19044b0697bb9541217fefacce4aa83190
TheCaffeine <85110314+TheCaffeine@users.noreply.github.com>
2022-01-10 18:42:26 +0300

    Create models.py

---
+++ models.py
@@ -0,0 +1,53 @@
+from django.db import models
+from django.contrib.auth.models import User
+from pyuploadcare.dj.models import ImageField
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+class NeighbourHood(models.Model):
+    name = models.CharField(max_length=50)
+    location = models.CharField(max_length=60)
+    admin = models.ForeignKey("Profile", on_delete=models.CASCADE, related_name='hood')
+    hood_logo = models.ImageField(upload_to='images/')
+    description = models.TextField()
+     def __str__(self):
+         return f'{self.name} hood'
+
+
+ class Profile(models.Model):
+     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+
+    
+          
+            
+    
+
+          
+          
+            
+    
+
+          
+    
+    @@ -46,5 +56,16 @@ class Business(models.Model):
+  
+    name = models.CharField(max_length=80, blank=True)
+    bio = models.TextField(max_length=254, blank=True)
+    profile_picture = models.ImageField(upload_to='images/', default='default.png')
+    neighbourhood = models.ForeignKey(NeighbourHood, on_delete=models.SET_NULL, null=True, related_name='members', blank=True)
+    def __str__(self):
+        return f'{self.user.username} profile'
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.profile.save()
+class Business(models.Model):
+    name = models.CharField(max_length=120)
+    email = models.EmailField(max_length=254)
+    description = models.TextField(blank=True)
+    neighbourhood = models.ForeignKey(NeighbourHood, on_delete=models.CASCADE, related_name='business')
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='owner')
+     def __str__(self):
+         return f'{self.name} Business'
