3d4c4d1cfa33ea12adf32112ad90e951341879fc
TheCaffeine <85110314+TheCaffeine@users.noreply.github.com>
2022-01-10 20:16:07 +0300

    adding logout functionality button

--- hood/views.py
+++ hood/views.py
@@ -1,12 +1,12 @@
-from django.shortcuts import render
- from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect
  from .forms import SignupForm
  from django.contrib.auth import login, authenticate
+ from django.contrib.auth.decorators import login_required
 
 
+ @login_required(login_url='login')
  def index(request):
      return render(request, 'index.html')
-
 
  def signup(request):
      return render(request, 'registration/signup.html')
