0b878bc70c6be57f9f92e794c412c64803114a0f
TheCaffeine <85110314+TheCaffeine@users.noreply.github.com>
2022-01-11 13:06:09 +0300

    Display all posts that belong to a specific neighbourhood

--- hood/admin.py
+++ hood/admin.py
@@ -1,8 +1,9 @@
-from django.contrib import admin
- from .models import Profile, NeighbourHood
+ from django.contrib import admin
  from .models import Profile, NeighbourHood, Business
+ from .models import Profile, NeighbourHood, Business, Post
 
 
  admin.site.register(Profile)
  admin.site.register(NeighbourHood)
  admin.site.register(Business)
+ admin.site.register(Post)
