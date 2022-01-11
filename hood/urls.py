d1f234fb67ff0c196f321c502411693b4f1eee2f
TheCaffeine <85110314+TheCaffeine@users.noreply.github.com>
2022-01-11 12:11:32 +0300

    view function

--- hood/urls.py
+++ hood/urls.py
@@ -10,4 +10,6 @@ urlpatterns = [
      path('profile/<username>/edit/', views.edit_profile, name='edit-profile')
      path('profile/<username>/edit/', views.edit_profile, name='edit-profile'),
      path('join_hood/<id>', views.join_hood, name='join-hood'),
+     path('leave_hood/<id>', views.leave_hood, name='leave-hood'),
+     path('single_hood/<hood_id>', views.single_hood, name='single-hood')
  ]
