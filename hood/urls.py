de670b8a2b83b9574531f5ed9bf7c009bcd7d7a3
TheCaffeine <85110314+TheCaffeine@users.noreply.github.com>
2022-01-11 13:08:31 +0300

    Display all posts that belong to a specific neighbourhood

--- hood/urls.py
+++ hood/urls.py
@@ -1,17 +1,16 @@
 from django.urls import path, include
 from . import views
-
 urlpatterns = [
-     path('', views.index, name='index'),
-     path('register/', views.signup, name='signup'),
-     path('account/', include('django.contrib.auth.urls')),
-     path('all-hoods/', views.hoods, name='hood'),
-     path('new-hood/', views.create_hood, name='new-hood'),
-     path('profile/<username>', views.profile, name='profile'),
-     path('profile/<username>/edit/', views.edit_profile, name='edit-profile'),
+    path('', views.index, name='index'),
+    path('register/', views.signup, name='signup'),
+    path('account/', include('django.contrib.auth.urls')),
+    path('all-hoods/', views.hoods, name='hood'),
+    path('new-hood/', views.create_hood, name='new-hood'),
+    path('profile/<username>', views.profile, name='profile'),
+    path('profile/<username>/edit/', views.edit_profile, name='edit-profile'),
      path('join_hood/<id>', views.join_hood, name='join-hood'),
      path('leave_hood/<id>', views.leave_hood, name='leave-hood'),
-     path('single_hood/<hood_id>', views.single_hood, name='single-hood')
      path('single_hood/<hood_id>', views.single_hood, name='single-hood'),
      path('new-post', views.create_post, name='post')
+     path('<hood_id>/new-post', views.create_post, name='post')
  ]
