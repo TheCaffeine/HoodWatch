9b41f7344c4f4f2cbf3a00134f2ce631f975308f
TheCaffeine <85110314+TheCaffeine@users.noreply.github.com>
2022-01-11 12:37:23 +0300

    Update views.py

--- hood/views.py
+++ hood/views.py
@@ -5,9 +5,11 @@ from django.contrib.auth.decorators import login_required
 from .models import NeighbourHood, Profile, Business
 from .forms import UpdateProfileForm, NeighbourHoodForm
 from django.contrib.auth.models import User
+
 @login_required(login_url='login')
 def index(request):
     return render(request, 'index.html')
+
 def signup(request):
     if request.method == 'POST':
         form = SignupForm(request.POST)
@@ -21,54 +23,62 @@ def signup(request):
     else:
         form = SignupForm()
     return render(request, 'registration/signup.html', {'form': form})
+
 def hoods(request):
     all_hoods = NeighbourHood.objects.all()
+    
     params = {
         'all_hoods': all_hoods,
     }
     return render(request, 'all_hoods.html', params)
+
 def create_hood(request):
     if request.method == 'POST':
         form = NeighbourHoodForm(request.POST, request.FILES)
         if form.is_valid():
-            hood = form.save(commit=False)
-            hood.admin = request.user.profile
-            hood.save()
-    else:
-        form = NeighbourHoodForm()
-    return render(request, 'newhood.html', {'form': form})
- def single_hood(request, hood_id):
-     hood = NeighbourHood.objects.get(id=hood_id)
-     business = Business.objects.filter(neighbourhood=hood)
-     form = BusinessForm()
-     if request.method == 'POST':
-         form = BusinessForm(request.POST)
-         if form.is_valid():
-             b_form = form.save(commit=False)
-             b_form.neighbourhood = hood
-             b_form.user = request.user.profile
-             b_form.save()
-             return redirect('single-hood', hood.id)
+             hood = form.save(commit=False)
+             hood.admin = request.user.profile
+             hood.save()
+             return redirect('hood')
      else:
-         form = BusinessForm()
-     params = {
-         'hood': hood,
-         'business': business,
+         form = NeighbourHoodForm()
+     return render(request, 'newhood.html', {'form': form})
+
+def single_hood(request, hood_id):
+    hood = NeighbourHood.objects.get(id=hood_id)
+    business = Business.objects.filter(neighbourhood=hood)
+    if request.method == 'POST':
+        form = BusinessForm(request.POST)
+        if form.is_valid():
+            b_form = form.save(commit=False)
+            b_form.neighbourhood = hood
+            b_form.user = request.user.profile
+            b_form.save()
+            return redirect('single-hood', hood.id)
+    else:
+        form = BusinessForm()
+    params = {
+        'hood': hood,
+        'business': business,
         'form': form
     }
     return render(request, 'single_hood.html', params)
+
 def join_hood(request, id):
     neighbourhood = get_object_or_404(NeighbourHood, id=id)
     request.user.profile.neighbourhood = neighbourhood
     request.user.profile.save()
     return redirect('hood')
+
 def leave_hood(request, id):
     hood = get_object_or_404(NeighbourHood, id=id)
     request.user.profile.neighbourhood = None
     request.user.profile.save()
     return redirect('hood')
 def profile(request, username):
+    
     return render(request, 'profile.html')
+
 def edit_profile(request, username):
     user = User.objects.get(username=username)
     if request.method == 'POST':
