2625cb51269dd70bee2e51d4ddb43def26a2ad34
TheCaffeine <85110314+TheCaffeine@users.noreply.github.com>
2022-01-11 11:25:38 +0300

    Create userprofile and edit profile templates

--- hoodwatch /views.py
+++ hoodwatch /views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import SignupForm
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.decorators import login_required
+from .models import NeighbourHood
+@login_required(login_url='login')
+def index(request):
+    return render(request, 'index.html')
+def signup(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('index')
+    else:
+        form = SignupForm()
+    return render(request, 'registration/signup.html', {'form': form})
+def hoods(request):
+    all_hoods = NeighbourHood.objects.all()
+    params = {
+         'all_hoods': all_hoods
+     }
+     return render(request, 'all_hoods.html', params)
 
- # Create your views here.
+
+ def profile(request, username):
+     return render(request, 'profile.html')
+
+
+ def edit_profile(request, username):
+     return render(request, 'editprofile.html')
