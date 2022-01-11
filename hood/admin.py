00c67f1fd1ee25a392f5b403e794b6112b3fb9f8
TheCaffeine <85110314+TheCaffeine@users.noreply.github.com>
2022-01-11 12:06:25 +0300

    Create new business model

--- hood/admin.py
+++ hood/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
- from .models import Profile
  from .models import Profile, NeighbourHood
+ from .models import Profile, NeighbourHood, Business
 
 
  admin.site.register(Profile)
  admin.site.register(NeighbourHood)
+ admin.site.register(Business)
