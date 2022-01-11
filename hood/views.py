5c6697b7dee6b4e9f19fbd347b4ac71d5c8a08c0
TheCaffeine <85110314+TheCaffeine@users.noreply.github.com>
2022-01-11 12:00:53 +0300

    Update views.py
    Create form to allow users to create new neighbourhood

--- hood/views.py
+++ hood/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .forms import SignupForm
-from django.contrib.auth import login, authenticate
-from django.contrib.auth.decorators import login_required
-from .models import NeighbourHood, Profile
-from .forms import UpdateProfileForm
-from django.contrib.auth.models import User
+ from django.contrib.auth import login, authenticate
+ from django.contrib.auth.decorators import login_required
+ from .models import NeighbourHood, Profile
+ from .forms import UpdateProfileForm
+ from .forms import UpdateProfileForm, NeighbourHoodForm
+ from django.contrib.auth.models import User
+
+
 @login_required(login_url='login')
 def index(request):
     return render(request, 'index.html')
@@ -22,21 +25,25 @@ def signup(request):
         form = SignupForm()
     return render(request, 'registration/signup.html', {'form': form})
 def hoods(request):
-    all_hoods = NeighbourHood.objects.all()
-    params = {
-        'all_hoods': all_hoods
-    }
-    return render(request, 'all_hoods.html', params)
+     all_hoods = NeighbourHood.objects.all()
+
+     params = {
+         'all_hoods': all_hoods
+         'all_hoods': all_hoods,
+     }
+     return render(request, 'all_hoods.html', params)
+
+
+ def create_hood(request):
+     form = NeighbourHoodForm()
+     return render(request, 'newhood.html', {'form': form})
+
 
  def join_hood(request, id):
      neighbourhood = get_object_or_404(NeighbourHood, id=id)
-     request.user.neighbourhood = neighbourhood
-     request.user.save()
      request.user.profile.neighbourhood = neighbourhood
-     request.user.profile.save()
-     return redirect('hood')
-
-
+    request.user.profile.save()
+    return redirect('hood')
 def profile(request, username):
     return render(request, 'profile.html')
 def edit_profile(request, username):
@@ -48,4 +55,4 @@ def edit_profile(request, username):
             return redirect('profile', user.username)
     else:
         form = UpdateProfileForm(instance=request.user.profile)
-    return render(request, 'editprofile.html', {'form': form}) 
+    return render(request, 'editprofile.html', {'form': form})
