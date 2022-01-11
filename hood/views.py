746f16eac02197fe8517511a819e36b503680146
TheCaffeine <85110314+TheCaffeine@users.noreply.github.com>
2022-01-11 11:45:20 +0300

    Create a view to allow a user to join a given neigbourhood

--- hood/views.py
+++ hood/views.py
@@ -1,17 +1,14 @@
- from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect
+ from django.shortcuts import render, redirect, get_object_or_404
  from .forms import SignupForm
  from django.contrib.auth import login, authenticate
  from django.contrib.auth.decorators import login_required
- from .models import NeighbourHood
- from .models import NeighbourHood, Profile
- from .forms import UpdateProfileForm
- from django.contrib.auth.models import User
-
-
- @login_required(login_url='login')
+from .models import NeighbourHood, Profile
+from .forms import UpdateProfileForm
+from django.contrib.auth.models import User
+@login_required(login_url='login')
 def index(request):
     return render(request, 'index.html')
- 
 def signup(request):
     if request.method == 'POST':
         form = SignupForm(request.POST)
@@ -25,25 +22,35 @@ def signup(request):
     else:
         form = SignupForm()
     return render(request, 'registration/signup.html', {'form': form})
-   
 def hoods(request):
     all_hoods = NeighbourHood.objects.all()
-  
     params = {
         'all_hoods': all_hoods
     }
-    return render(request, 'all_hoods.html', params)
-   
-def profile(request, username):
-    return render(request, 'profile.html')
+     return render(request, 'all_hoods.html', params)
+
+
+ def join_hood(request, id):
+     neighbourhood = get_object_or_404(NeighbourHood, id=id)
+     request.user.neighbourhood = neighbourhood
+     request.user.save()
+     return redirect('hood')
+
+
+ def profile(request, username):
+     return render(request, 'profile.html')
 
 
  def edit_profile(request, username):
-     form = UpdateProfileForm()
      user = User.objects.get(username=username)
      if request.method == 'POST':
          form = UpdateProfileForm(request.POST)
 
+         form = UpdateProfileForm(request.POST, request.FILES, instance=request.user.profile)
+         if form.is_valid():
+             form.save()
+             return redirect('profile', user.username)
      else:
          form = UpdateProfileForm()
+         form = UpdateProfileForm(instance=request.user.profile)
      return render(request, 'editprofile.html', {'form': form})
