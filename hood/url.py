7119433554582e34f8a0ebc08304670aa92b9e0b
TheCaffeine <85110314+TheCaffeine@users.noreply.github.com>
2022-01-11 11:15:33 +0300

    Create all-hoods template to display all the neighbourhoods

--- hood/url.py
+++ hood/url.py
@@ -2,8 +2,8 @@ from django.urls import path, include
  from . import views
 
 urlpatterns = [
-     path('', views.index, name='index')
      path('', views.index, name='index'),
-     path('register/', views.SignupForm, name='signup'),
+     path('register/', views.signup, name='signup'),
      path('account/', include('django.contrib.auth.urls')),
+     path('all-hoods/', views.hoods, name='hood')
  ] 
--- hood/views.py
+++ hood/views.py
@@ -1,24 +1,31 @@
-from django.shortcuts import render, redirect
- from .forms import SignupForm
+from .forms import SignupForm
  from django.contrib.auth import login, authenticate
  from django.contrib.auth.decorators import login_required
+ from .models import NeighbourHood
 
 
  @login_required(login_url='login')
- def index(request):
-     return render(request, 'index.html')
-
- def signup(request):
-     return render(request, 'registration/signup.html')
-     if request.method == 'POST':
-         form = SignupForm(request.POST)
-         if form.is_valid():
-             form.save()
-             username = form.cleaned_data.get('username')
-             password = form.cleaned_data.get('password1')
-             user = authenticate(username=username, password=password)
-             login(request, user)
-             return redirect('index')
+def index(request):
+    return render(request, 'index.html')
+def signup(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('index')
      else:
          form = SignupForm()
      return render(request, 'registration/signup.html', {'form': form})
+
+
+ def hoods(request):
+     all_hoods = NeighbourHood.objects.all()
+
+     params = {
+         'all_hoods': all_hoods
+     }
+     return render(request, 'all_hoods.html', params)
