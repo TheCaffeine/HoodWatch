589aee95e0efbcb4152f29af46d3d72da3f2a607
TheCaffeine <85110314+TheCaffeine@users.noreply.github.com>
2022-01-11 11:53:19 +0300

    Create form to allow users to create new neighbourhood

--- hood/forms.py
+++ hood/forms.py
@@ -1,17 +1,25 @@
- from django import forms
+from django import forms
  from django.contrib.auth.forms import UserCreationForm
  from django.contrib.auth.models import User
  from .models import Profile
+ from .models import Profile, NeighbourHood
+ from pyuploadcare.dj.forms import ImageField
 
 
  class SignupForm(UserCreationForm):
     email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
-     class Meta:
-         model = User
-         fields = ('username', 'email', 'password1', 'password2')
-
-
- class UpdateProfileForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
+class UpdateProfileForm(forms.ModelForm):
      class Meta:
          model = Profile
          exclude = ('user', 'neighbourhood')
+
+
+ class NeighbourHoodForm(forms.ModelForm):
+     photo = ImageField(label='')
+
+     class Meta:
+         model = NeighbourHood
+         fields = ('logo', 'name', 'location', 'description')
