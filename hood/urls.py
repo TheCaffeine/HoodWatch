c8c8adba7bdf5a02e39b926fd5aa1067a1884462
TheCaffeine <85110314+TheCaffeine@users.noreply.github.com>
2022-01-11 13:08:47 +0300

    Update urls.py

--- hood/urls.py
+++ hood/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path, include
 from . import views
+
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('register/', views.signup, name='signup'),
-    path('account/', include('django.contrib.auth.urls')),
-    path('all-hoods/', views.hoods, name='hood'),
-    path('new-hood/', views.create_hood, name='new-hood'),
-    path('profile/<username>', views.profile, name='profile'),
-    path('profile/<username>/edit/', views.edit_profile, name='edit-profile'),
+     path('', views.index, name='index'),
+     path('register/', views.signup, name='signup'),
+     path('account/', include('django.contrib.auth.urls')),
+     path('all-hoods/', views.hoods, name='hood'),
+     path('new-hood/', views.create_hood, name='new-hood'),
+     path('profile/<username>', views.profile, name='profile'),
+     path('profile/<username>/edit/', views.edit_profile, name='edit-profile'),
      path('join_hood/<id>', views.join_hood, name='join-hood'),
      path('leave_hood/<id>', views.leave_hood, name='leave-hood'),
      path('single_hood/<hood_id>', views.single_hood, name='single-hood'),
