c0777cb85bc966607e58ce1c127a4f21ed3a4315
TheCaffeine <85110314+TheCaffeine@users.noreply.github.com>
2022-01-11 11:43:42 +0300

    Create a view to allow a user to join a given neigbourhood

--- hood/urls.py
+++ hood/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
 from . import views
-
 urlpatterns = [
-     path('', views.index, name='index'),
-     path('register/', views.signup, name='signup'),
+    path('', views.index, name='index'),
+    path('register/', views.signup, name='signup'),
      path('account/', include('django.contrib.auth.urls')),
-     path('all-hoods/', views.hoods, name='hood')
      path('all-hoods/', views.hoods, name='hood'),
      path('profile/<username>', views.profile, name='profile'),
      path('profile/<username>/edit/', views.edit_profile, name='edit-profile')
+     path('profile/<username>/edit/', views.edit_profile, name='edit-profile'),
+     path('join_hood/<id>', views.join_hood, name='join-hood'),
+ ]
