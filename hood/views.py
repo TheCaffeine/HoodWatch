2d5cee68e2273f72a1477957f0cd37d55972939a
TheCaffeine <85110314+TheCaffeine@users.noreply.github.com>
2022-01-11 11:28:11 +0300

    Update views.py

--- hood/views.py
+++ hood/views.py
@@ -5,9 +5,10 @@
 
 
  @login_required(login_url='login')
-def index(request):
+ def index(request):
     return render(request, 'index.html')
-def signup(request):
+  
+ def signup(request):
     if request.method == 'POST':
         form = SignupForm(request.POST)
         if form.is_valid():
