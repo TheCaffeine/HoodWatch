143f192cec3ff9ba6f4bfd87ba3df0385b80faad
TheCaffeine <85110314+TheCaffeine@users.noreply.github.com>
2022-01-11 14:30:08 +0300

    Update models.py

--- hood/models.py
+++ hood/models.py
@@ -11,26 +11,23 @@ class NeighbourHood(models.Model):
     admin = models.ForeignKey("Profile", on_delete=models.CASCADE, related_name='hood')
     hood_logo = models.ImageField(upload_to='images/')
     description = models.TextField()
-    health = models.IntegerField(null=True, blank=True)
-    police = models.IntegerField(null=True, blank=True)
     health_tell = models.IntegerField(null=True, blank=True)
     police_number = models.IntegerField(null=True, blank=True)
-    health = models.IntegerField(null=True, blank=True)
-    police = models.IntegerField(null=True, blank=True)
-    
+
     def __str__(self):
         return f'{self.name} hood'
-    
+
     def create_neighborhood(self):
         self.save()
-        
+
     def delete_neighborhood(self):
         self.delete()
-        
+
     @classmethod
     def find_neighborhood(cls, neighborhood_id):
         return cls.objects.filter(id=neighborhood_id)
-    
+
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
     name = models.CharField(max_length=80, blank=True)
@@ -38,43 +35,44 @@ class Profile(models.Model):
     profile_picture = models.ImageField(upload_to='images/', default='default.png')
     location = models.CharField(max_length=50, blank=True, null=True)
     neighbourhood = models.ForeignKey(NeighbourHood, on_delete=models.SET_NULL, null=True, related_name='members', blank=True)
-    
+
     def __str__(self):
         return f'{self.user.username} profile'
+
     @receiver(post_save, sender=User)
-    
     def create_user_profile(sender, instance, created, **kwargs):
         if created:
             Profile.objects.create(user=instance)
-            
+
     @receiver(post_save, sender=User)
     def save_user_profile(sender, instance, **kwargs):
         instance.profile.save()
-        
+
+
 class Business(models.Model):
     name = models.CharField(max_length=120)
     email = models.EmailField(max_length=254)
     description = models.TextField(blank=True)
     neighbourhood = models.ForeignKey(NeighbourHood, on_delete=models.CASCADE, related_name='business')
     user = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='owner')
-    
+
     def __str__(self):
         return f'{self.name} Business'
-    
+
     def create_business(self):
         self.save()
-        
+
     def delete_business(self):
         self.delete()
-        
+
     @classmethod
     def find_business(cls, business_id):
-         return cls.objects.filter(id=business_id)
+        return cls.objects.filter(id=business_id)
 
 
-
- class Post(models.Model):
-     post = models.TextField()
-     date = models.DateTimeField(auto_now_add=True)
-     user = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='post_owner')
-     hood = models.ForeignKey(NeighbourHood, on_delete=models.CASCADE, related_name='hood_post')
+class Post(models.Model):
+    title = models.CharField(max_length=120, null=True)
+    post = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='post_owner')
+    hood = models.ForeignKey(NeighbourHood, on_delete=models.CASCADE, related_name='hood_post')
