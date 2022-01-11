5b219c69a19e1bf3a9e3b8e8d787b08385372a5c
TheCaffeine <85110314+TheCaffeine@users.noreply.github.com>
2022-01-11 11:20:55 +0300

    Update urls.py

--- hoodwatch /urls.py
+++ hoodwatch /urls.py
@@ -1,4 +1,4 @@
-from django.conf import settings
+ from django.conf import settings
  from django.conf.urls.static import static
  from django.contrib import admin
  from django.urls import path, include
